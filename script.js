02f2bb15dcdee7259c15e9e59678e7cf6982d740
Anton Öhman <anton.ohman@hotmail.com>
2019-11-01 13:30:30 +0100

    Added .toLowerCase

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
+// button.onclick = startQuiz
+
+
 function startQuiz() {
-    let quiz1 = prompt("Vad blir 506 + 831?")
-    let quiz2 = prompt("Vad bör man inte ha i chokladbollar?")
-    let quiz3 = prompt("Vem föreställer jag?")
-    let quiz4 = prompt("Är du nykter?")
-    let correctAnswers = 0
+  let quiz1 = prompt("Vad blir 506 + 831?").toLowerCase()
+  let quiz2 = prompt("Vad bör man inte ha i chokladbollar?").toLowerCase()
+  let quiz3 = prompt("Vem föreställer jag?").toLowerCase()
+  let quiz4 = prompt("Är du nykter?").toLowerCase()
+  let correctAnswers = 0
 
-if (quiz1 == "1337" || quiz1 == 1337) {
+  if (quiz1 === "1337") {
+    correctAnswers += 1
+  //  doThis()
+ // } else if (quiz1 === "1080") {
+ //   doThat()
+  }
+
+  if (quiz2 === "marsipan") {
     correctAnswers += 1
   }
 
-if (quiz2 == "marsipan" || quiz2 == "Marsipan") {
-    correctAnswers += 1
-  }
-  
-if (quiz3 == "Michael Jackson" || quiz3 == "michael jackson" || quiz3 == "Micke J") {
+  if (quiz3 === "michael jackson" || quiz3 === "micke j" || quiz3 === "mj") {
     correctAnswers += 1
   }
 
-if (quiz4 == "Ja" || quiz4 == "ja") {
+  if (quiz4 === "ja") {
     correctAnswers += 1
   }
 
-let prisonRealese = 0;
-  if (correctAnswers == 4) {
+  let prisonRealese = 0;
+  if (correctAnswers === 4) {
     prisonrealese = alert("Fantastiskt, du är fri!")
 
   } else if (correctAnswers <= 3) {
     prisonrealese = alert("Vad i hela friden? Försök igen...")
   }
-  
+
 }
 
 // const resultsContainer = document.getElementById('results')
